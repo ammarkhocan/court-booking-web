66ef4d8d8552bf6b63276ae49cfcccf764ab7590
Ammar Khocan <ammarkhocan02@gmail.com>
2026-09-14 00:04:19 +0700

    feat: register router and fix layout main sticky footer

--- app/layouts/layout-main.tsx
+++ app/layouts/layout-main.tsx
@@ -1,18 +1,33 @@
-import { Outlet } from "react-router";
+import { Link, Outlet } from "react-router";
 
 export default function LayoutMain() {
   const year = new Date().getFullYear();
 
   return (
-    <div>
+    <div className="flex min-h-screen flex-col">
       <nav>
-        <h1>
-          NAVBAR
-          <img src="" alt="" />
-        </h1>
+        <ul className="flex gap-10 items-center">
+          <li>
+            <h1>LOGO</h1>
+          </li>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/courts">Court</Link>
+          </li>
+          <li>
+            <Link to="/booking">Booking</Link>
+          </li>
+          <li>
+            <Link to="/login">login</Link>
+          </li>
+        </ul>
       </nav>
 
-      <Outlet />
+      <main className="flex-1">
+        <Outlet />
+      </main>
 
       <footer>
         <p>&copy; {year} Court Booking</p>
