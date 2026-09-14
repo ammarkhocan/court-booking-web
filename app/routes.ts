660d73ad1cf0855b47decacbb3031246d13b1155
Ammar Khocan <ammarkhocan02@gmail.com>
2026-09-14 08:25:24 +0700

    fix: correct route parameter syntax for court details

--- app/routes.ts
+++ app/routes.ts
@@ -9,7 +9,7 @@ export default [
   layout("layouts/layout-main.tsx", [
     index("routes/home.tsx"),
     route("/courts", "routes/courts.tsx"),
-    route("/courts/{:id}", "routes/courts-id.tsx"),
+    route("/courts/:id", "routes/courts-id.tsx"),
     route("/register", "routes/register.tsx"),
     route("/login", "routes/login.tsx"),
     route("/dashboard", "routes/dashboard.tsx"),
