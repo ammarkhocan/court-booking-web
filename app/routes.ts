5be05a1670fa407dadb957ece51adaef63d715be
Ammar Khocan <ammarkhocan02@gmail.com>
2026-09-14 07:41:40 +0700

    feat: add routes for courts, registration, login, and dashboard

--- app/routes.ts
+++ app/routes.ts
@@ -8,6 +8,12 @@ import {
 export default [
   layout("layouts/layout-main.tsx", [
     index("routes/home.tsx"),
+    route("/courts", "routes/courts.tsx"),
+    route("/courts/{:id}", "routes/courts-id.tsx"),
+    route("/register", "routes/register.tsx"),
+    route("/login", "routes/login.tsx"),
+    route("/dashboard", "routes/dashboard.tsx"),
+
     // route("/court", "routes/court.tsx"),
   ]),
 ] satisfies RouteConfig;
